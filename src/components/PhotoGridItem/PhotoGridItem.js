441ba5aea17aef9e5ae5e3f025258cc4dc8291a8
Sabrina <sabrina@semgrep.com>
2023-11-29 13:54:00 +0100

    responsive image format, change fit to cover

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -2,10 +2,23 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
+  const srcRoot = src.replace('.jpg', '');
+  const avifSrcSet = `${srcRoot}.avif 1x, ${srcRoot}@2x.avif 2x, ${srcRoot}@3x.avif 3x`;
+  const jpgSrcSet = `${srcRoot}.jpg 1x, ${srcRoot}@2x.jpg 2x, ${srcRoot}@3x.jpg 3x`;
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source
+            type="image/avif"
+            srcset={avifSrcSet}
+          />
+          <source
+            type="image/avif"
+            srcset={jpgSrcSet}
+          />
+          <Image src={src} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,6 +41,7 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
